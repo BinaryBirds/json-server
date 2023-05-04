dcf5e5359c66787ed60cc637b5fab36204df59dd
Tibor Bodecs <mail.tib@gmail.com>
2023-05-04 12:04:31 +0200

    cors

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -4,6 +4,22 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) throws {
+
+    let corsConfiguration = CORSMiddleware.Configuration(
+        allowedOrigin: .all,
+        allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+        allowedHeaders: [
+            .accept,
+            .authorization,
+            .contentType,
+            .origin,
+            .xRequestedWith,
+            .userAgent,
+            .accessControlAllowOrigin
+        ]
+    )
+    let cors = CORSMiddleware(configuration: corsConfiguration)
+    app.middleware.use(cors, at: .beginning)
     
     app.middleware.use(SleepMiddleware())
     app.middleware.use(ChaosMiddleware())
